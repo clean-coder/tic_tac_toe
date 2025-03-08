c81c00a74ea69564e25504322b4c4aa665e1539b
clean-coder <jean-claude.brantschen@gmx.ch>
2025-03-08 08:39:00 +0100

    refactoring: create and init Player[] in one step

--- src/main/java/ch/jduke/Grid.java
+++ src/main/java/ch/jduke/Grid.java
@@ -1,16 +1,14 @@
 package ch.jduke;
 
 public class Grid {
-    private Player[] grid;
+    private Player[] grid = {
+            Player.None, Player.None, Player.None,
+            Player.None, Player.None, Player.None,
+            Player.None, Player.None, Player.None
+    };
+
     private WinningPositions winningPositions = new WinningPositions();
 
-    public Grid() {
-        grid = new Player[]{
-                Player.None, Player.None, Player.None,
-                Player.None, Player.None, Player.None,
-                Player.None, Player.None, Player.None
-        };
-    }
 
     public void mark(Position position, Player actualPlayer) {
         grid[position.ordinal()] = actualPlayer;
