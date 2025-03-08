3127eacc402a0eb60e6f823319478ec65cf42966
clean-coder <jean-claude.brantschen@gmx.ch>
2025-03-08 09:02:01 +0100

    refactoring: fix typo

--- src/main/java/ch/jduke/WinningPositions.java
+++ src/main/java/ch/jduke/WinningPositions.java
@@ -7,10 +7,10 @@ public class WinningPositions {
     private WinningPosition rightDiagonal = new WinningPosition(Position.TopRight, Position.MiddleCenter, Position.BottomLeft);
     private WinningPosition topRow =    new WinningPosition(Position.TopLeft,    Position.TopCenter,    Position.TopRight);
     private WinningPosition middleRow = new WinningPosition(Position.MiddleLeft, Position.MiddleCenter, Position.MiddleRight);
-    private WinningPosition bottemRow = new WinningPosition(Position.BottomLeft, Position.BottomCenter, Position.BottomRight);
+    private WinningPosition bottomRow = new WinningPosition(Position.BottomLeft, Position.BottomCenter, Position.BottomRight);
 
     private List<WinningPosition> allWinningPositions = List.of(
-            leftDiagonal, rightDiagonal, topRow, middleRow, bottemRow
+            leftDiagonal, rightDiagonal, topRow, middleRow, bottomRow
     );
 
     public WinningState checkWin(Player[] grid) {
